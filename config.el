0c3477fdc745676209a5b4fecbbfb4d41bcd367f
Maciej Kalisiak <maciejk@maciejk-macbookpro.roam.corp.google.com>
2020-01-17 15:31:28 -0500

    sync

--- config.el
+++ config.el
@@ -18,7 +18,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Input Mono" :size 14))
+(setq doom-font (font-spec :family "Input Mono" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -39,8 +39,9 @@
 
 ;;; Agenda
   (setq
-        ;;org-agenda-span 1
-        org-agenda-start-on-weekday 1
+        org-agenda-span 1
+        org-agenda-start-day "."
+        ;;org-agenda-start-on-weekday 1
         org-agenda-window-setup 'only-window
         org-agenda-tags-column 'auto
         org-agenda-sorting-strategy '(time-up todo-state-down priority-down)
