ea6fe5dbfaad4e71484582edf3dcdb163c370767
Maciej Kalisiak <mkalisiak@gmail.com>
2021-09-30 09:09:35 -0400

    Disable the separate frame for company
    It causes issues on MacOS when Emacs is fullscreened.

--- init.el
+++ init.el
@@ -21,7 +21,8 @@
 
        :completion
        (company          ; the ultimate code completion backend
-        +childframe)
+       ; +childframe)
+       )
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ivy               ; a search engine for love and life
